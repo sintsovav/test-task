9ab960169cc02f3dac32f27e237eba0d9549cbf0
sintsov.av <anatolij.v.sincov2@bspb.ru>
2022-03-05 03:48:40 +0300

    added acl for provisioning instance

--- terraform/04-vpc.tf
+++ terraform/04-vpc.tf
@@ -6,21 +6,30 @@ resource "aws_network_acl" "deny_all" {
   vpc_id = aws_vpc.flask-app-test-vpc.id
 
   egress {
-    protocol   = "tcp"
+    protocol   = "-1"
     rule_no    = 200
     action     = "deny"
     cidr_block = "0.0.0.0/0"
     from_port  = 0
-    to_port    = 65535
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 150
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
   }
 
   ingress {
     protocol   = "tcp"
     rule_no    = 100
-    action     = "deny"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 65535
+    action     = "allow"
+    cidr_block = "172.16.10.101/32"
+    from_port  = 22
+    to_port    = 22
   }
 
 }
